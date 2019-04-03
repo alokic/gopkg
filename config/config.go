c61819653c9dc25097b504392620b14565d6765c
Alok Srivastava <aloks@qubole.com>
2019-04-03 19:06:45 +0530

    done

--- config/config.go
+++ config/config.go
@@ -1,8 +1,9 @@
-package v2
+package config
 
 import (
 	"flag"
 	"fmt"
+	"os"
 	"reflect"
 	"strings"
 
@@ -18,25 +19,25 @@ type Logger interface {
 	Log(...interface{}) error
 }
 
-// viper will by default read config.[ext] in Folders directory.
+// viper will by default read config.[ext] in folders directory.
 type config struct {
-	Folders   []string
-	EnvPrefix string
+	folders   []string
+	envPrefix string
 	cfgStruct interface{}
+	flags     *pflag.FlagSet
+	v         *viper.Viper
 }
 
-//NewConfig constructor for config.
-func NewConfig(envPrefix string, folders ...string) *config {
-	return &config{Folders: folders, EnvPrefix: envPrefix}
+// New constructor for config.
+func New(cfg interface{}, envPrefix string, flags *pflag.FlagSet, folders ...string) *config {
+	return &config{cfgStruct: cfg, folders: folders, envPrefix: envPrefix, flags: flags, v: viper.New()}
 }
 
 //Load will setup the config object passed by reading configurations from different sources like env, cmd line flag, config file.
-func (c *config) Load(cfg interface{}) error {
+func (c *config) Load() error {
 	if err := c.setupViper(); err != nil {
 		return errors.Wrap(err, "error in setting up viper")
 	}
-
-	c.cfgStruct = cfg
 
 	iter, err := structutils.NewIterator(c.cfgStruct, []string{"json", "required", "usage"})
 	if err != nil {
@@ -49,15 +50,15 @@ func (c *config) Load(cfg interface{}) error {
 			break
 		}
 
-		c.setDefaultConfig(field)
+		c.setDefaultValue(field)
 		err = c.setFlag(field)
 		if err != nil {
 			return err
 		}
 	}
 
-	c.bindFlagsViper()
-	return c.setConfig()
+	c.parseFlags()
+	return c.populateStruct()
 }
 
 // Print config.
@@ -98,22 +99,26 @@ func (c *config) setFlag(field *structutils.Field) error {
 		ut += " (optional)"
 	}
 
+	// get default or environment value
+	defaultVal := c.v.Get(jt)
+
 	switch field.Type {
 	case "string":
-		flag.String(jt, typeutils.ToStr(field.Value), ut)
+		flag.String(jt, typeutils.ToStr(defaultVal), ut)
 	case "int":
-		flag.Int(jt, typeutils.ToInt(field.Value), ut)
+		flag.Int(jt, typeutils.ToInt(defaultVal), ut)
 	case "float64":
-		flag.Float64(jt, typeutils.ToFloat64(field.Value), ut)
+		flag.Float64(jt, typeutils.ToFloat64(defaultVal), ut)
 	case "uint64":
-		flag.Uint64(jt, typeutils.ToUint64(field.Value), ut)
+		flag.Uint64(jt, typeutils.ToUint64(defaultVal), ut)
 	case "bool":
-		flag.Bool(jt, typeutils.ToBool(field.Value), ut)
+		flag.Bool(jt, typeutils.ToBool(defaultVal), ut)
 	}
+
 	return nil
 }
 
-func (c *config) setDefaultConfig(field *structutils.Field) {
+func (c *config) setDefaultValue(field *structutils.Field) {
 	if typeutils.Blank(field.Value) {
 		return
 	}
@@ -123,7 +128,7 @@ func (c *config) setDefaultConfig(field *structutils.Field) {
 		return
 	}
 
-	viper.SetDefault(key, field.Value)
+	c.v.SetDefault(key, field.Value)
 }
 
 func (c *config) getTag(field *structutils.Field, tag string) string {
@@ -136,30 +141,33 @@ func (c *config) getTag(field *structutils.Field, tag string) string {
 }
 
 func (c *config) setupViper() error {
-	viper.SetEnvPrefix(c.EnvPrefix)
-	viper.AutomaticEnv()
+	if c.envPrefix != "" {
+		viper.SetEnvPrefix(c.envPrefix)
+	}
+
+	c.v.AutomaticEnv()
 	return c.setConfigPath()
 }
 
 func (c *config) setConfigPath() error {
-	if len(c.Folders) == 0 {
+	if len(c.folders) == 0 {
 		return nil
 	}
 
-	for _, folder := range c.Folders {
-		viper.AddConfigPath(folder)
+	for _, folder := range c.folders {
+		c.v.AddConfigPath(folder)
 	}
 
-	return viper.ReadInConfig()
+	return c.v.ReadInConfig()
 }
 
-func (c *config) bindFlagsViper() {
-	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
-	pflag.Parse()
-	viper.BindPFlags(pflag.CommandLine)
+func (c *config) parseFlags() {
+	c.flags.AddGoFlagSet(flag.CommandLine)
+	c.v.BindPFlags(c.flags)
+	c.flags.Parse(os.Args[1:])
 }
 
-func (c *config) setConfig() error {
+func (c *config) populateStruct() error {
 	iter, err := structutils.NewIterator(c.cfgStruct, []string{"json", "required"})
 	if err != nil {
 		return err
@@ -174,9 +182,9 @@ func (c *config) setConfig() error {
 
 		key := c.getTag(field, "json")
 
-		value := viper.Get(key)
+		value := c.v.Get(key)
 		if typeutils.Blank(value) && c.isRequired(field) {
-			return fmt.Errorf("%s is required config. Either pass as --%s cmd-line args or set %s_%s as environment variable", field.Name, key, strings.ToUpper(c.EnvPrefix), strings.ToUpper(key))
+			return fmt.Errorf("%s is required config. Either pass as --%s cmd-line args or set %s as environment variable", field.Name, key, c.envKey(key))
 		}
 
 		err := c.setStructField(field, value)
@@ -215,10 +223,16 @@ func (c *config) setStructField(field *structutils.Field, value interface{}) err
 	case "bool":
 		f.SetBool(typeutils.ToBool(value))
 	}
-
 	return nil
 }
 
 func (c *config) isRequired(field *structutils.Field) bool {
 	return c.getTag(field, "required") == "true"
+}
+
+func (c *config) envKey(key string) string {
+	if c.envPrefix != "" {
+		return strings.ToUpper(c.envPrefix) + "_" + strings.ToUpper(key)
+	}
+	return strings.ToUpper(key)
 }
