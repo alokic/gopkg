6d61b67d002a735565ff343bad30b242903f3c25
Alok Srivastava <aloks@qubole.com>
2019-04-26 13:58:58 +0530

    added sliceutils

--- sliceutils/intersect.go
+++ sliceutils/intersect.go
@@ -14,10 +14,21 @@ func Intersect(a, b interface{}, fn Eqaute) interface{} {
 
 	for i := 0; i < av.Len(); i++ {
 		el := av.Index(i).Interface()
-		if fn(b, el) {
+		if contains(b, el, fn) {
 			set = append(set, el)
 		}
 	}
 
 	return set
+}
+
+func contains(a interface{}, e interface{}, fn Eqaute) bool {
+	v := reflect.ValueOf(a)
+
+	for i := 0; i < v.Len(); i++ {
+		if fn(v.Index(i).Interface(), e) {
+			return true
+		}
+	}
+	return false
 }
